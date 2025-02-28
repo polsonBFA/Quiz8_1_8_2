3ddac953e1e7525a310a6557575df84270d86d31
Peter Olson <peter.olson@bfacademy.de>
2025-02-28 12:12:17 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,25 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println(computeScore("quake"));
+    //write code that will declare and initialize a 2-D Array called seatChart
+
+
+
+    //Test each of your methods by calling them from the main method.
+
+    
   }
 
+  //Write a method called getRandomStudent(seatChart) that returns the name of a randomly selected student from the seating chart
 
-  /* Question 3: In Scrabble®, different letters are
-   * assigned different numbers of points:Write a method
-   * computeScore(String word) that returns the score for a
-   * word without using any if statements. */
+
+  //Write a method called printChart(seatChart) that prints out the seating chart.
+
+  
+
+  /*[Extension task] Overload your getRandomStudent method so that you pass in an additional integer parameter 
+    for the number of random students that you want to select. The method should return an arrayList with the 
+    number of students without duplicates. Precondition - number of random students selected will not be larger than class size.*/
+
   
 
   
